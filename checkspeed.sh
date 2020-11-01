92438ba11373e1650025845f6281bb19b24fbe83
Sorin Negulescu <sorin.negulescu@gmail.com>
2020-11-01 23:37:26 +0200

    Update checkspeed.sh

--- checkspeed.sh
+++ checkspeed.sh
@@ -4,5 +4,5 @@
 # put in /usr/local/bin (by copying beep.sh) or make sure is executable
 #
 # Checks speed of the internet connection using the speedtest package
-
+# Outputs ping (ms), download (MB/s), upload (MB/s)
 speedtest --server 7682 | awk -v FS="(: | ms| Mbit/s)" '{print $2}' | tr '\n' ',' | sed 's/^.....//' | sed 's/.\{2\}$//' | sed 's/,\{2,\}/,/g' | logger -t speedtest
