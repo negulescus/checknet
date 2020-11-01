ad4c1cd1e9c61187d203a7c76168576a12bcd6dd
Sorin Negulescu <sorin.negulescu@gmail.com>
2020-11-01 23:35:39 +0200

    Add files via upload

---
+++ checkspeed.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# speedcheck.sh
+# put in /usr/local/bin (by copying beep.sh) or make sure is executable
+#
+# Checks speed of the internet connection using the speedtest package
+
+speedtest --server 7682 | awk -v FS="(: | ms| Mbit/s)" '{print $2}' | tr '\n' ',' | sed 's/^.....//' | sed 's/.\{2\}$//' | sed 's/,\{2,\}/,/g' | logger -t speedtest
